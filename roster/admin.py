059282dbaa1d8256ffb05e19171e46447512320c
haruto.ubuntu <haruto-cs@outlook.com>
2025-09-20 13:33:32 +0900

    add models in roster app to admin site

--- roster/admin.py
+++ roster/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Favorite, Player, Sport, Team
+
+admin.site.register(Player)
+admin.site.register(Sport)
+admin.site.register(Team)
+admin.site.register(Comment)
+admin.site.register(Favorite)
