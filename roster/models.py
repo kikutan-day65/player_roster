050c7e79820cb04a3ccd6e273d26f9fa714bf859
haruto.ubuntu <haruto-cs@outlook.com>
2025-09-20 12:57:52 +0900

    add team model

--- roster/models.py
+++ roster/models.py
@@ -3,14 +3,12 @@ from uuid import uuid4
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from core.validators import OnlyLettersValidator
+from core.validators import only_letters_validator
 
 
 class Player(models.Model):
-    only_letters_validator = OnlyLettersValidator()
-
     id = models.UUIDField(_("id"), primary_key=True, default=uuid4, editable=False)
-    # team_id(FK)
+    team = models.ForeignKey("Team", on_delete=models.CASCADE, related_name="players")
     first_name = models.CharField(
         _("first_name"),
         max_length=150,
@@ -38,4 +36,31 @@ class Player(models.Model):
         null=True,
         blank=True,
         help_text=_("Timestamp of when the player was deleted"),
+    )
+
+
+class Team(models.Model):
+    id = models.UUIDField(_("id"), primary_key=True, default=uuid4, editable=False)
+    # sport_id (FK)
+    name = models.CharField(
+        _("team_name"),
+        max_length=150,
+        help_text=_("Required. 150 characters or fewer. Letters only."),
+        validators=[only_letters_validator],
+    )
+    created_at = models.DateTimeField(
+        _("created at"),
+        auto_now_add=True,
+        help_text=_("Timestamp of when the team was created"),
+    )
+    updated_at = models.DateTimeField(
+        _("updated at"),
+        auto_now=True,
+        help_text=_("Timestamp of when the team was updated"),
+    )
+    deleted_at = models.DateTimeField(
+        _("deleted at"),
+        null=True,
+        blank=True,
+        help_text=_("Timestamp of when the team was deleted"),
     )
