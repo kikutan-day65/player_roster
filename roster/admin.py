1071a6c7209275ec28ee609eaa1dc4998e0fd6ff
haruto.ubuntu <haruto-cs@outlook.com>
2025-09-25 22:35:14 +0900

    remove sport model from admin site

--- roster/admin.py
+++ roster/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from .models import Comment, Favorite, Player, Sport, Team
+from .models import Comment, Favorite, Player, Team
 
 admin.site.register(Player)
-admin.site.register(Sport)
 admin.site.register(Team)
 admin.site.register(Comment)
 admin.site.register(Favorite)
