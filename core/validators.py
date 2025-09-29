c6ac6182e5e2af9fe6f819edc379724e9d04db76
haruto.ubuntu <haruto-cs@outlook.com>
2025-09-29 21:25:16 +0900

    fix validator to allow space in the team name

--- core/validators.py
+++ core/validators.py
@@ -1,5 +1,5 @@
 from django.core.validators import RegexValidator
 
 only_letters_validator = RegexValidator(
-    regex=r"^[A-Za-z]+$", message="Only letters are allowed."
+    regex=r"^[A-Za-z ]+$", message="Only letters are allowed."
 )
