80a3ab7c055b3a902bf4987213c36ae7acfc19c1
haruto.ubuntu <haruto-cs@outlook.com>
2025-10-08 19:46:30 +0900

    add create test for team viewset

--- tests/roster/views/test_roster_create.py
+++ tests/roster/views/test_roster_create.py
@@ -77,3 +77,63 @@ def test_player_creation_fails_without_required_fields(
 
     assert response.status_code == 400
     assert field in response.data
+
+
+@pytest.mark.django_db
+def test_team_creation_success(api_client, test_team_data, admin_user):
+    expected_fields = {
+        "id",
+        "name",
+        "sport",
+        "created_at",
+        "updated_at",
+        "deleted_at",
+    }
+
+    api_client.force_authenticate(admin_user)
+
+    url = reverse("team-list")
+    response = api_client.post(url, data=test_team_data)
+
+    assert response.status_code == 201
+    assert response.data["name"] == test_team_data["name"]
+    assert response.data["sport"] == test_team_data["sport"]
+    assert set(response.data.keys()) == expected_fields
+
+
+@pytest.mark.django_db
+def test_fails_to_create_team_by_general_user(api_client, test_team_data, test_user):
+    api_client.force_authenticate(test_user)
+
+    url = reverse("team-list")
+    response = api_client.post(url, data=test_team_data)
+
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_fails_to_create_team_by_unauthenticated_user(api_client, test_team_data):
+
+    url = reverse("team-list")
+    response = api_client.post(url, data=test_team_data)
+
+    assert response.status_code == 401
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "field, value",
+    [("name", ""), ("sport", "")],
+    ids=["without name", "without sport"],
+)
+def test_team_creation_fails_without_required_fields(
+    api_client, test_team_data, admin_user, field, value
+):
+    api_client.force_authenticate(admin_user)
+    test_team_data[field] = value
+
+    url = reverse("team-list")
+    response = api_client.post(url, data=test_team_data)
+
+    assert response.status_code == 400
+    assert field in response.data
