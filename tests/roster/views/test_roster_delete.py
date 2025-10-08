12a304c734d44f2c22883ef10a52847edeb7670e
haruto.ubuntu <haruto-cs@outlook.com>
2025-10-08 20:28:12 +0900

    add delete test for team viewset

--- tests/roster/views/test_roster_delete.py
+++ tests/roster/views/test_roster_delete.py
@@ -59,3 +59,60 @@ def test_fails_to_delete_nonexistent_player(api_client, super_user):
     response = api_client.delete(url)
 
     assert response.status_code == 404
+
+
+@pytest.mark.django_db
+def test_successes_to_delete_team(api_client, test_teams, super_user):
+    api_client.force_authenticate(super_user)
+    team = test_teams[0]
+    url = reverse("team-detail", kwargs={"pk": team.id})
+
+    response = api_client.delete(url)
+
+    assert response.status_code == 204
+
+    team.refresh_from_db()
+    assert team.deleted_at is not None
+
+
+@pytest.mark.django_db
+def test_fails_to_delete_team_by_admin_user(api_client, test_teams, admin_user):
+    api_client.force_authenticate(admin_user)
+    team = test_teams[0]
+    url = reverse("team-detail", kwargs={"pk": team.id})
+
+    response = api_client.delete(url)
+
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_fails_to_delete_team_by_general_user(api_client, test_teams, test_user):
+    api_client.force_authenticate(test_user)
+    team = test_teams[0]
+    url = reverse("team-detail", kwargs={"pk": team.id})
+
+    response = api_client.delete(url)
+
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_fails_to_delete_team_by_unauthenticated_user(api_client, test_teams):
+    team = test_teams[0]
+    url = reverse("team-detail", kwargs={"pk": team.id})
+
+    response = api_client.delete(url)
+
+    assert response.status_code == 401
+
+
+@pytest.mark.django_db
+def test_fails_to_delete_nonexistent_team(api_client, super_user):
+    api_client.force_authenticate(super_user)
+    team_id = uuid4()
+    url = reverse("team-detail", kwargs={"pk": team_id})
+
+    response = api_client.delete(url)
+
+    assert response.status_code == 404
