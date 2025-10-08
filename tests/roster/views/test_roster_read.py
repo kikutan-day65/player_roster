a542235dff4a1e46a587c8484243a4430f04c6e5
haruto.ubuntu <haruto-cs@outlook.com>
2025-10-08 20:00:42 +0900

    add read test for team viewset

--- tests/roster/views/test_roster_read.py
+++ tests/roster/views/test_roster_read.py
@@ -99,3 +99,74 @@ def test_fails_to_get_the_nonexistent_player(api_client):
     response = api_client.get(url)
 
     assert response.status_code == 404
+
+
+@pytest.mark.django_db
+def test_success_to_get_list_of_all_teams(api_client, test_teams):
+    expected_fields = {"id", "name", "sport"}
+
+    url = reverse("team-list")
+    response = api_client.get(url)
+
+    assert response.status_code == 200
+    assert len(response.data) == 2
+    assert set(response.data[0].keys()) == expected_fields
+
+
+@pytest.mark.django_db
+def test_success_to_get_list_of_all_teams_by_staff(api_client, test_teams, admin_user):
+    expected_fields = {
+        "id",
+        "name",
+        "sport",
+        "created_at",
+        "updated_at",
+        "deleted_at",
+    }
+
+    api_client.force_authenticate(admin_user)
+    url = reverse("team-list")
+    response = api_client.get(url)
+
+    assert response.status_code == 200
+    assert len(response.data) == 2
+    assert set(response.data[0].keys()) == expected_fields
+
+
+@pytest.mark.django_db
+def test_success_to_get_specific_team(api_client, test_teams):
+    expected_fields = {"id", "name", "sport"}
+
+    url = reverse("team-detail", kwargs={"pk": test_teams[0].id})
+    response = api_client.get(url)
+
+    assert response.status_code == 200
+    assert set(response.data.keys()) == expected_fields
+
+
+@pytest.mark.django_db
+def test_success_to_get_specific_team_by_staff(api_client, test_teams, admin_user):
+    expected_fields = {
+        "id",
+        "name",
+        "sport",
+        "created_at",
+        "updated_at",
+        "deleted_at",
+    }
+
+    api_client.force_authenticate(admin_user)
+    url = reverse("team-detail", kwargs={"pk": test_teams[0].id})
+    response = api_client.get(url)
+
+    assert response.status_code == 200
+    assert set(response.data.keys()) == expected_fields
+
+
+@pytest.mark.django_db
+def test_fails_to_get_the_nonexistent_team(api_client):
+    nonexistent_team_id = uuid4()
+    url = reverse("team-detail", kwargs={"pk": nonexistent_team_id})
+    response = api_client.get(url)
+
+    assert response.status_code == 404
