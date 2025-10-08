4645fc9d06fe9989d988c6feca92c0e8cb41de82
haruto.ubuntu <haruto-cs@outlook.com>
2025-10-08 19:45:46 +0900

    add urls for team viewset

--- roster/urls.py
+++ roster/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import PlayerViewSet
+from .views import PlayerViewSet, TeamViewSet
 
 router = DefaultRouter()
 router.register(r"players", PlayerViewSet, basename="player")
+router.register(r"teams", TeamViewSet, basename="team")
 
 urlpatterns = [
     path("", include(router.urls)),
