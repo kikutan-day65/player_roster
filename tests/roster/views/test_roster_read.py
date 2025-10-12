c8902766f4c4937d367522211f5a2347b7ce1a4f
haruto.ubuntu <haruto-cs@outlook.com>
2025-10-12 10:42:51 +0900

    fix crud tests of player and team

--- tests/roster/views/test_roster_read.py
+++ tests/roster/views/test_roster_read.py
@@ -5,24 +5,36 @@ from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_success_to_get_list_of_all_players(api_client, test_players):
-    expected_fields = {"id", "team", "first_name", "last_name"}
-    expected_fields_for_team = {"id", "name", "sport"}
+def test_success_to_get_list_of_all_players(api_client, test_players, test_comments):
+    player_fields = {"id", "first_name", "last_name", "comments", "team"}
+    comment_fields = {"id", "body", "created_at", "user"}
+    user_fields = {"id", "username"}
+    team_fields = {"id", "name"}
 
     url = reverse("player-list")
     response = api_client.get(url)
 
     assert response.status_code == 200
     assert len(response.data) == 2
-    assert set(response.data[0].keys()) == expected_fields
-    assert set(response.data[0]["team"].keys()) == expected_fields_for_team
+
+    player_data = response.data[0]
+    assert set(player_data.keys()) == player_fields
+
+    comment_data = player_data["comments"][0]
+    assert set(comment_data.keys()) == comment_fields
+
+    user_data = comment_data["user"]
+    assert set(user_data.keys()) == user_fields
+
+    team_data = player_data["team"]
+    assert set(team_data.keys()) == team_fields
 
 
 @pytest.mark.django_db
 def test_success_to_get_list_of_all_players_by_staff(
-    api_client, test_players, admin_user
+    api_client, test_players, admin_user, test_comments
 ):
-    expected_fields = {
+    player_fields = {
         "id",
         "team",
         "first_name",
@@ -30,15 +42,11 @@ def test_success_to_get_list_of_all_players_by_staff(
         "created_at",
         "updated_at",
         "deleted_at",
+        "comments",
     }
-    expected_fields_for_team = {
-        "id",
-        "name",
-        "sport",
-        "created_at",
-        "updated_at",
-        "deleted_at",
-    }
+    comment_fields = {"id", "body", "created_at", "user"}
+    user_fields = {"id", "username"}
+    team_fields = {"id", "name"}
 
     api_client.force_authenticate(admin_user)
     url = reverse("player-list")
@@ -46,26 +54,50 @@ def test_success_to_get_list_of_all_players_by_staff(
 
     assert response.status_code == 200
     assert len(response.data) == 2
-    assert set(response.data[0].keys()) == expected_fields
-    assert set(response.data[0]["team"].keys()) == expected_fields_for_team
+
+    player_data = response.data[0]
+    assert set(player_data.keys()) == player_fields
+
+    comment_data = player_data["comments"][0]
+    assert set(comment_data.keys()) == comment_fields
+
+    user_data = comment_data["user"]
+    assert set(user_data.keys()) == user_fields
+
+    team_data = player_data["team"]
+    assert set(team_data.keys()) == team_fields
 
 
 @pytest.mark.django_db
-def test_success_to_get_specific_player(api_client, test_players):
-    expected_fields = {"id", "team", "first_name", "last_name"}
-    expected_fields_for_team = {"id", "name", "sport"}
+def test_success_to_get_specific_player(api_client, test_players, test_comments):
+    player_fields = {"id", "first_name", "last_name", "comments", "team"}
+    comment_fields = {"id", "body", "created_at", "user"}
+    user_fields = {"id", "username"}
+    team_fields = {"id", "name"}
 
     url = reverse("player-detail", kwargs={"pk": test_players[0].id})
     response = api_client.get(url)
 
     assert response.status_code == 200
-    assert set(response.data.keys()) == expected_fields
-    assert set(response.data["team"].keys()) == expected_fields_for_team
+
+    player_data = response.data
+    assert set(player_data.keys()) == player_fields
+
+    comment_data = player_data["comments"][0]
+    assert set(comment_data.keys()) == comment_fields
+
+    user_data = comment_data["user"]
+    assert set(user_data.keys()) == user_fields
+
+    team_data = player_data["team"]
+    assert set(team_data.keys()) == team_fields
 
 
 @pytest.mark.django_db
-def test_success_to_get_specific_player_by_staff(api_client, test_players, admin_user):
-    expected_fields = {
+def test_success_to_get_specific_player_by_staff(
+    api_client, test_players, admin_user, test_comments
+):
+    player_fields = {
         "id",
         "team",
         "first_name",
@@ -73,23 +105,29 @@ def test_success_to_get_specific_player_by_staff(api_client, test_players, admin
         "created_at",
         "updated_at",
         "deleted_at",
+        "comments",
     }
-    expected_fields_for_team = {
-        "id",
-        "name",
-        "sport",
-        "created_at",
-        "updated_at",
-        "deleted_at",
-    }
+    comment_fields = {"id", "body", "created_at", "user"}
+    user_fields = {"id", "username"}
+    team_fields = {"id", "name"}
 
     api_client.force_authenticate(admin_user)
     url = reverse("player-detail", kwargs={"pk": test_players[0].id})
     response = api_client.get(url)
 
     assert response.status_code == 200
-    assert set(response.data.keys()) == expected_fields
-    assert set(response.data["team"].keys()) == expected_fields_for_team
+
+    player_data = response.data
+    assert set(player_data.keys()) == player_fields
+
+    comment_data = player_data["comments"][0]
+    assert set(comment_data.keys()) == comment_fields
+
+    user_data = comment_data["user"]
+    assert set(user_data.keys()) == user_fields
+
+    team_data = player_data["team"]
+    assert set(team_data.keys()) == team_fields
 
 
 @pytest.mark.django_db
@@ -103,19 +141,21 @@ def test_fails_to_get_the_nonexistent_player(api_client):
 
 @pytest.mark.django_db
 def test_success_to_get_list_of_all_teams(api_client, test_teams):
-    expected_fields = {"id", "name", "sport"}
+    team_fields = {"id", "name", "sport"}
 
     url = reverse("team-list")
     response = api_client.get(url)
 
     assert response.status_code == 200
     assert len(response.data) == 2
-    assert set(response.data[0].keys()) == expected_fields
+
+    team_data = response.data[0]
+    assert set(team_data.keys()) == team_fields
 
 
 @pytest.mark.django_db
 def test_success_to_get_list_of_all_teams_by_staff(api_client, test_teams, admin_user):
-    expected_fields = {
+    team_fields = {
         "id",
         "name",
         "sport",
@@ -130,23 +170,27 @@ def test_success_to_get_list_of_all_teams_by_staff(api_client, test_teams, admin
 
     assert response.status_code == 200
     assert len(response.data) == 2
-    assert set(response.data[0].keys()) == expected_fields
+
+    team_data = response.data[0]
+    assert set(team_data.keys()) == team_fields
 
 
 @pytest.mark.django_db
 def test_success_to_get_specific_team(api_client, test_teams):
-    expected_fields = {"id", "name", "sport"}
+    team_fields = {"id", "name", "sport"}
 
     url = reverse("team-detail", kwargs={"pk": test_teams[0].id})
     response = api_client.get(url)
 
     assert response.status_code == 200
-    assert set(response.data.keys()) == expected_fields
+
+    team_data = response.data
+    assert set(team_data.keys()) == team_fields
 
 
 @pytest.mark.django_db
 def test_success_to_get_specific_team_by_staff(api_client, test_teams, admin_user):
-    expected_fields = {
+    team_fields = {
         "id",
         "name",
         "sport",
@@ -160,7 +204,9 @@ def test_success_to_get_specific_team_by_staff(api_client, test_teams, admin_use
     response = api_client.get(url)
 
     assert response.status_code == 200
-    assert set(response.data.keys()) == expected_fields
+
+    team_data = response.data
+    assert set(team_data.keys()) == team_fields
 
 
 @pytest.mark.django_db
