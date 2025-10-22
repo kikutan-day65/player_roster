d98dc96bdc107a1d72d57478ad9e7e9bc825e599
haruto.ubuntu <haruto-cs@outlook.com>
2025-10-22 21:41:52 +0900

    add comment viewset

--- roster/views.py
+++ roster/views.py
@@ -99,3 +99,43 @@ class PlayerViewSet(viewsets.ModelViewSet):
     def perform_destroy(self, instance):
         instance.deleted_at = timezone.now()
         instance.save(update_fields=["deleted_at"])
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    http_method_names = ["get", "post", "patch", "delete"]
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Comment.objects.all().order_by("-created_at")
+        return Comment.objects.filter(deleted_at__isnull=True).order_by("-created_at")
+
+    def get_permissions(self):
+        if self.action == "create":
+            permission_classes = [IsAuthenticated]
+        elif self.action in ["list", "retrieve"]:
+            permission_classes = [AllowAny]
+        elif self.action == "partial_update":
+            permission_classes = [IsAdminUser | IsAuthenticatedOwner]
+        elif self.action == "destroy":
+            permission_classes = [IsSuperUser | IsAuthenticatedOwner]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return CommentCreateSerializer
+        elif self.action in ["list", "retrieve"]:
+            if self.request.user.is_staff:
+                return CommentListRetrieveAdminSerializer
+            return CommentListRetrievePublicSerializer
+        elif self.action == "partial_update":
+            return CommentPatchSerializer
+        return CommentListRetrievePublicSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        instance.deleted_at = timezone.now()
+        instance.save(update_fields=["deleted_at"])
