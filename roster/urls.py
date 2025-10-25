0e253080cd9d5402193778164597b712d13a159a
haruto.ubuntu <haruto-cs@outlook.com>
2025-10-25 15:19:46 +0900

    add team_player router

--- roster/urls.py
+++ roster/urls.py
@@ -1,13 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+from rest_framework_nested.routers import NestedSimpleRouter
 
-from .views import CommentViewSet, PlayerViewSet, TeamViewSet
+from .views import CommentViewSet, PlayerViewSet, TeamPlayerViewSet, TeamViewSet
 
 router = SimpleRouter()
 router.register(r"teams", TeamViewSet, basename="team")
 router.register(r"players", PlayerViewSet, basename="player")
 router.register(r"comments", CommentViewSet, basename="comment")
 
+team_player_router = NestedSimpleRouter(router, r"teams", lookup="team")
+team_player_router.register(r"players", TeamPlayerViewSet, basename="team_player")
+
+
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(team_player_router.urls)),
 ]
