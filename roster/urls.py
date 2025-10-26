6a55dd6bce033dfc42f37f5aaa9c444e199d716f
haruto.ubuntu <haruto-cs@outlook.com>
2025-10-26 19:24:35 +0900

    add player_comment router

--- roster/urls.py
+++ roster/urls.py
@@ -2,7 +2,13 @@ from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 from rest_framework_nested.routers import NestedSimpleRouter
 
-from .views import CommentViewSet, PlayerViewSet, TeamPlayerViewSet, TeamViewSet
+from .views import (
+    CommentViewSet,
+    PlayerCommentViewSet,
+    PlayerViewSet,
+    TeamPlayerViewSet,
+    TeamViewSet,
+)
 
 router = SimpleRouter()
 router.register(r"teams", TeamViewSet, basename="team")
@@ -12,8 +18,14 @@ router.register(r"comments", CommentViewSet, basename="comment")
 team_player_router = NestedSimpleRouter(router, r"teams", lookup="team")
 team_player_router.register(r"players", TeamPlayerViewSet, basename="team_player")
 
+player_comment_router = NestedSimpleRouter(router, r"players", lookup="player")
+player_comment_router.register(
+    r"comments", PlayerCommentViewSet, basename="player_comment"
+)
+
 
 urlpatterns = [
     path("", include(router.urls)),
     path("", include(team_player_router.urls)),
+    path("", include(player_comment_router.urls)),
 ]
