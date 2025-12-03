c719c78ed258d1110346bf122e1a62b0862eb17c
haruto.ubuntu <haruto-cs@outlook.com>
2025-12-03 19:22:32 +0900

    fix urls due to changing of views for user account

--- user_account/urls.py
+++ user_account/urls.py
@@ -1,29 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from rest_framework_nested.routers import NestedSimpleRouter
 
-from .views import (
-    MeCommentListView,
-    MeCommentRetrieveUpdateDestroyView,
-    MeRetrieveUpdateDestroyView,
-    UserAccountCommentViewSet,
-    UserAccountViewSet,
-)
+from .views import MeAPIView, MeCommentAPIView, UserAccountViewSet
 
 router = SimpleRouter()
 router.register(r"user-accounts", UserAccountViewSet, basename="user_account")
 
-user_comment_router = NestedSimpleRouter(router, r"user-accounts", lookup="user")
-user_comment_router.register(
-    r"comments", UserAccountCommentViewSet, basename="user_account_comment"
-)
-
 # fmt: off
 urlpatterns = [
-    path("user-accounts/me/", MeRetrieveUpdateDestroyView.as_view(), name="me"),
-    path("user-accounts/me/comments/", MeCommentListView.as_view(), name="me_comment_list"),
-    path("user-accounts/me/comments/<uuid:pk>/", MeCommentRetrieveUpdateDestroyView.as_view(), name="me_comment_detail"),
+    path("user-accounts/me/", MeAPIView.as_view(), name="me"),
+    path("user-accounts/me/comments/", MeCommentAPIView.as_view(), name="me_comments"),
     path("", include(router.urls)),
-    path("", include(user_comment_router.urls)),
 ]
 # fmt: on
