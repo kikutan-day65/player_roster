1621d8531daccae2821b4468b14a5c9b39e1419c
haruto.ubuntu <haruto-cs@outlook.com>
2025-12-08 19:29:22 +0900

    add CommentViewSet

--- roster/views.py
+++ roster/views.py
@@ -157,21 +157,23 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         if self.request.user.is_staff:
-            return Comment.objects.all().order_by("-created_at")
-        return Comment.objects.filter(deleted_at__isnull=True).order_by("-created_at")
+            qs = Comment.objects.all()
+        else:
+            qs = Comment.objects.filter(deleted_at__isnull=True)
+        return qs.order_by("-created_at")
 
     def get_permissions(self):
         if self.action == "create":
-            permission_classes = [IsAuthenticated]
+            permission_class = [IsAuthenticated]
         elif self.action in ["list", "retrieve"]:
-            permission_classes = [AllowAny]
+            permission_class = [AllowAny]
         elif self.action == "partial_update":
-            permission_classes = [IsAdminUser | IsAuthenticatedOwner]
+            permission_class = [IsAdminUser | IsAuthenticatedOwner]
         elif self.action == "destroy":
-            permission_classes = [IsSuperUser | IsAuthenticatedOwner]
+            permission_class = [IsSuperUser | IsAuthenticatedOwner]
         else:
-            permission_classes = [IsAuthenticated]
-        return [permission() for permission in permission_classes]
+            permission_class = [IsAuthenticated]
+        return [permission() for permission in permission_class]
 
     def get_serializer_class(self):
         if self.action == "create":
@@ -186,91 +188,6 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-    def perform_destroy(self, instance):
-        instance.soft_delete()
-
-
-class TeamPlayerViewSet(viewsets.ModelViewSet):
-    http_method_names = ["post", "get", "patch", "delete"]
-
-    def get_queryset(self):
-        if self.request.user.is_staff:
-            return Player.objects.filter(team=self.kwargs["team_pk"]).order_by(
-                "-created_at"
-            )
-        return Player.objects.filter(
-            team=self.kwargs["team_pk"], deleted_at__isnull=True
-        ).order_by("-created_at")
-
-    def get_permissions(self):
-        if self.action in ["create", "partial_update"]:
-            permission_classes = [IsAdminUser]
-        elif self.action in ["list", "retrieve"]:
-            permission_classes = [AllowAny]
-        elif self.action == "destroy":
-            permission_classes = [IsSuperUser]
-        else:
-            permission_classes = [IsAuthenticated]
-
-        return [permission() for permission in permission_classes]
-
-    def get_serializer_class(self):
-        if self.action == "create":
-            return TeamPlayerCreateSerializer
-        elif self.action in ["list", "retrieve"]:
-            if self.request.user.is_staff:
-                return TeamPlayerListRetrieveAdminSerializer
-            return TeamPlayerListRetrievePublicSerializer
-        elif self.action == "partial_update":
-            return TeamPlayerPatchSerializer
-        return TeamPlayerListRetrievePublicSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(team=self.kwargs["team_pk"])
-
-    def perform_destroy(self, instance):
-        instance.soft_delete()
-
-
-class PlayerCommentViewSet(viewsets.ModelViewSet):
-    http_method_names = ["post", "get", "patch", "delete"]
-
-    def get_queryset(self):
-        if self.request.user.is_staff:
-            return Comment.objects.filter(player=self.kwargs["player_pk"]).order_by(
-                "-created_at"
-            )
-        return Comment.objects.filter(
-            player=self.kwargs["player_pk"], deleted_at__isnull=True
-        ).order_by("-created_at")
-
-    def get_permissions(self):
-        if self.action == "create":
-            permission_classes = [IsAuthenticated]
-        elif self.action in ["list", "retrieve"]:
-            permission_classes = [AllowAny]
-        elif self.action == "partial_update":
-            permission_classes = [IsAdminUser | IsAuthenticatedOwner]
-        elif self.action == "destroy":
-            permission_classes = [IsSuperUser | IsAuthenticatedOwner]
-        else:
-            permission_classes = [IsAuthenticated]
-        return [permission() for permission in permission_classes]
-
-    def get_serializer_class(self):
-        if self.action == "create":
-            return PlayerCommentCreateSerializer
-        elif self.action in ["list", "retrieve"]:
-            if self.request.user.is_staff:
-                return PlayerCommentListRetrieveAdminSerializer
-            return PlayerCommentListRetrievePublicSerializer
-        elif self.action == "partial_update":
-            return PlayerCommentPatchSerializer
-        return PlayerCommentListRetrievePublicSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user, player=self.kwargs["player_pk"])
 
     def perform_destroy(self, instance):
         instance.soft_delete()
