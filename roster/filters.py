a4f4a7e59414dee907407f431057c6419b03bf14
haruto.ubuntu <haruto-cs@outlook.com>
2025-12-17 20:32:01 +0900

    fix filters to use BaseFilterSet

--- roster/filters.py
+++ roster/filters.py
@@ -1,44 +1,30 @@
-import django_filters as filters
+from django_filters import rest_framework as filters
+
+from core.filters import BaseFilterSet
 
 from .models import Comment, Player, Team
 
 
-class TeamFilter(filters.FilterSet):
+class TeamFilter(BaseFilterSet):
     sport = filters.ChoiceFilter(field_name="sport", choices=Team.SportChoice)
     name = filters.CharFilter(field_name="name", lookup_expr="icontains")
-    created_at_date = filters.DateFilter(field_name="created_at", lookup_expr="date")
-    created_at_year = filters.NumberFilter(field_name="created_at", lookup_expr="year")
-    created_at_year_gte = filters.NumberFilter(
-        field_name="created_at", lookup_expr="year__gte"
-    )
-    created_at_year_lte = filters.NumberFilter(
-        field_name="created_at", lookup_expr="year__lte"
-    )
 
     class Meta:
         model = Team
         fields = []
 
 
-class PlayerFilter(filters.FilterSet):
+class PlayerFilter(BaseFilterSet):
     team_name = filters.CharFilter(field_name="team__name", lookup_expr="icontains")
     first_name = filters.CharFilter(field_name="first_name", lookup_expr="icontains")
     last_name = filters.CharFilter(field_name="last_name", lookup_expr="icontains")
-    created_at_date = filters.DateFilter(field_name="created_at", lookup_expr="date")
-    created_at_year = filters.NumberFilter(field_name="created_at", lookup_expr="year")
-    created_at_year_gte = filters.NumberFilter(
-        field_name="created_at", lookup_expr="year__gte"
-    )
-    created_at_year_lte = filters.NumberFilter(
-        field_name="created_at", lookup_expr="year__lte"
-    )
 
     class Meta:
         model = Player
         fields = []
 
 
-class CommentFilter(filters.FilterSet):
+class CommentFilter(BaseFilterSet):
     user_username = filters.CharFilter(
         field_name="user__username", lookup_expr="icontains"
     )
@@ -47,14 +33,6 @@ class CommentFilter(filters.FilterSet):
     )
     player_last_name = filters.CharFilter(
         field_name="player__last_name", lookup_expr="icontains"
-    )
-    created_at_date = filters.DateFilter(field_name="created_at", lookup_expr="date")
-    created_at_year = filters.NumberFilter(field_name="created_at", lookup_expr="year")
-    created_at_year_gte = filters.NumberFilter(
-        field_name="created_at", lookup_expr="year__gte"
-    )
-    created_at_year_lte = filters.NumberFilter(
-        field_name="created_at", lookup_expr="year__lte"
     )
 
     class Meta:
